ee4f39fcbf4511965d429492a2c85878847268a2
shugad <emilycarter20001@gmail.com>
2022-04-25 12:32:57 +0300

    Update ALLOWED_HOSTS with site and development server URL

--- ecology/settings.py
+++ ecology/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecology-russia.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
